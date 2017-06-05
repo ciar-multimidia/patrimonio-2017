5f5034ccc6b593507655fd182fd72694081b86e7
vitaozim <victorhugocgodoi@gmail.com>
2017-06-05 12:58:51 -0300

    Arrumando JS
    Mas nada feito por enquanto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,36 @@
 jQuery(document).ready(function($) {
-	$('body.livro1').prepend('\
+
+	var corpo = $('body');
+	var titulosVolumes = [
+		"Volume 1: Identidade e Patrimônio Culturais",
+		"Volume 2: Acesso e Participação à Vida cultural &bull; Informação e Comunicação &bull; Responsabilidade dos Atores Públicos",
+		"Volume 3: Educação e Formação &bull; Cooperação Cultural &bull; Princípios de Administração Democrática",
+		"Volume 4: Acesso e Participação 1a Vida Cultural &bull; Informação e Comunicação"
+	];
+
+	var templateHeader = $('\
 		<header id="topo">\
 			<div class="container">\
 				<a href="../../../index.html" class="marca">\
 					<span>Curso de Especialização em</span>\
 					<span class="curso-nome">Patrimônio, Direitos Culturais e Cidadania</span>\
-					<span class="livro">Volume 1: Identidade e Patrimônio Culturais</span>\
-				</a>\
-				<a href="../../../sumario.html" class="sumario"><span class="menu"><span>&mdash;</span><span>&mdash;</span><span>&mdash;</span></span>Sumário</a>\
-			</div>\
-		</header>\
-	');
-	$('body.livro2').prepend('\
-		<header id="topo">\
-			<div class="container">\
-				<a href="../../../index.html" class="marca">\
-					<span>Curso de Especialização em</span>\
-					<span class="curso-nome">Patrimônio, Direitos Culturais e Cidadania</span>\
-					<span class="livro">Volume 2: Acesso e Participação à Vida cultural &bull; Informação e Comunicação &bull; Responsabilidade dos Atores Públicos</span>\
-				</a>\
-				<a href="../../../sumario.html" class="sumario"><span class="menu"><span>&mdash;</span><span>&mdash;</span><span>&mdash;</span></span>Sumário</a>\
-			</div>\
-		</header>\
-	');
-	$('body.livro3').prepend('\
-		<header id="topo">\
-			<div class="container">\
-				<a href="../../../index.html" class="marca">\
-					<span>Curso de Especialização em</span>\
-					<span class="curso-nome">Patrimônio, Direitos Culturais e Cidadania</span>\
-					<span class="livro">Volume 3: Educação e Formação &bull; Cooperação Cultural &bull; Princípios de Administração Democrática</span>\
-				</a>\
-				<a href="../../../sumario.html" class="sumario"><span class="menu"><span>&mdash;</span><span>&mdash;</span><span>&mdash;</span></span>Sumário</a>\
-			</div>\
-		</header>\
-	');
-	$('body.livro4').prepend('\
-		<header id="topo">\
-			<div class="container">\
-				<a href="../../../index.html" class="marca">\
-					<span>Curso de Especialização em</span>\
-					<span class="curso-nome">Patrimônio, Direitos Culturais e Cidadania</span>\
-					<span class="livro">Volume 4: Acesso e Participação 1a Vida Cultural &bull; Informação e Comunicação</span>\
+					<span class="livro"></span>\
 				</a>\
 				<a href="../../../sumario.html" class="sumario"><span class="menu"><span>&mdash;</span><span>&mdash;</span><span>&mdash;</span></span>Sumário</a>\
 			</div>\
 		</header>\
 	');
 
-	$('#rodape > .container').append('<a href="#subir" class="btn-subir">subir</a>');
-	$('a.btn-subir').on('click', function(){
+	$.each(titulosVolumes, function(index, val) {
+		 if (corpo.hasClass('livro'+(index+1) ) ) {
+		 	clone = templateHeader.clone();
+		 	clone.find('span.livro').html(val);
+		 	corpo.prepend(clone);
+		 }
+	});
+	
+
+	$('#rodape > .container').append('<a href="#subir" class="btn-subir">subir</a>').on('click', function(){
 	  $('html, body').animate({scrollTop:0},1100);
 	  return false;
 	});	
