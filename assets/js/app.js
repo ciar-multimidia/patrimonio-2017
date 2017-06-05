ec00a33398bf1d649e2df553ac1b90de47f9cb82
vitaozim <victorhugocgodoi@gmail.com>
2017-06-05 17:09:26 -0300

    Codigo das notas de rodapé em andamento

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,8 +30,29 @@ jQuery(document).ready(function($) {
 	});
 	
 
-	$('#rodape > .container').append('<a href="#subir" class="btn-subir">subir</a>').on('click', function(){
+	$('#rodape > .container').append('<a href="#subir" class="btn-subir">subir</a>');
+	$('a.btn-subir').on('click', function(){
 	  $('html, body').animate({scrollTop:0},1100);
 	  return false;
 	});	
+
+	var notasrodape = $('.nota-rodape');
+	var btrodape = $('.numero-rodape');
+
+	btrodape.on('click', function(event) {
+		var clicado = $(this).attr('data-numero');
+		var posTop = $(this).position().top;
+		var aSair = notasrodape.filter('.db');
+		aSair.removeClass('visivel');
+		setTimeout(function(){
+			aSair.removeClass('db');
+		},400);
+		var aRevelar = notasrodape.filter('[data-numero=\''+clicado+'\']');
+		console.log(aRevelar);
+		aRevelar.addClass('db').css('top', posTop);
+		setTimeout(function(){
+			aRevelar.addClass('visivel');
+		},20);
+		
+	});
 });
