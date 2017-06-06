a412d5432e7a09677cd8a27c3563c565328e78f7
vitaozim <victorhugocgodoi@gmail.com>
2017-06-06 17:05:20 -0300

    Fancybox + rodape finalizado + muitas outras coisas!
    Nota de rodape QUASE finalizado na verdade. Não quero ele fechando quando clicar na mesma nota.
    
    Adição dos arquivos do fancybox
    
    Adição de importação do fancybox nos htmls
    
    Adição do CSS do fancybox
    
    Aplicação do Fancybox nos anexos

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,23 +8,112 @@ jQuery(document).ready(function($) {
 
 	var notasrodape = $('.nota-rodape');
 	var btrodape = $('.numero-rodape');
-	var rodapeAtual;
+
+	// função que define as posições das notas de rodapé.
+	var attPosRodape = function(){
+		notasrodape.each(function(index, el) {
+			var datanumero = $(el).attr('data-numero');
+			var btEquivalente = btrodape.filter('[data-numero=\''+datanumero+'\']');
+
+			var novoTop = (function(){
+				var topCalculado = btEquivalente.position().top - $(el).outerHeight()/2;
+				if (topCalculado < 0) {
+					return 1;
+				} else if(topCalculado > ( $(el).closest('article').height() + $(el).height() ) ){
+					return ($(el).closest('article').height() + $(el).height() - 1);
+				} else{
+					return topCalculado;
+				}
+				
+			})();
+			$(el).css('top', novoTop);
+
+		});	
+	}
+	attPosRodape();
+
+
+
+	// Atualizar posições sempre que o usuário redimensionar a janela do navegador.
+	$(window).on('resize', function(event) {
+		attPosRodape();
+	});
+
+
+
+	var rodapeAtual = 0;
+
+	notasrodape.each(function(index, el) {
+		$(el).children().wrapAll('<div class="posrel"><div class="texto"></div></div>');
+		$(el).find('.posrel').prepend('<p class="numero-equivalente"></p><button class="fechar"></button>');
+		$(el).find('.numero-equivalente').text($(el).attr('data-numero'));
+		$(el).find('button.fechar').on('click', function(event) {
+			$(this).closest('.nota-rodape').removeClass('visivel');
+			setTimeout(function(){
+				$(this).closest('.nota-rodape').removeClass('db');
+				rodapeAtual = 0;
+				
+			},400);
+		});
+
+	});	
 
 	btrodape.on('click', function(event) {
 
-		var clicado = $(this).attr('data-numero');
-		var posTop = $(this).position().top;
-		var aSair = notasrodape.filter('.db');
-		aSair.removeClass('visivel');
-		setTimeout(function(){
-			aSair.removeClass('db');
-		},400);
-		var aRevelar = notasrodape.filter('[data-numero=\''+clicado+'\']');
-		console.log(aRevelar);
-		aRevelar.addClass('db').css('top', posTop);
-		setTimeout(function(){
-			aRevelar.addClass('visivel');
-		},20);
-		
+		var numeroClicado = parseInt( $(this).attr('data-numero') );
+
+		if (rodapeAtual !== 0) {
+
+			var rodapeAFechar = notasrodape.filter('[data-numero=\''+rodapeAtual+'\']');
+			rodapeAFechar.removeClass('visivel');
+			setTimeout(function(){
+				rodapeAFechar.removeClass('db');
+				rodapeAFechar = aRevelar;
+
+			},400);
+		}
+
+		var aRevelar = notasrodape.filter('[data-numero=\''+numeroClicado+'\']');
+
+		if (numeroClicado !== rodapeAtual) {
+			aRevelar.addClass('db');
+			setTimeout(function(){
+				aRevelar.addClass('visivel');
+			},20);
+
+			rodapeAtual = parseInt( $(this).attr('data-numero') );
+		} else{
+			rodapeAtual = 0;
+		}
 	});
+
+
+	var conjAnexos = $('div.anexos');
+	var nMaxAnexos = 8;
+
+	conjAnexos.each(function(index, el) {
+		var anexos = $(el).children('figure');
+		if (anexos.length > nMaxAnexos) {
+			var excessosAnexos = anexos.length - nMaxAnexos;
+			anexos.eq(nMaxAnexos-1).append('<p class="maisanexos">+'+excessosAnexos+'</p>')
+		}
+
+		var fancyboxArgs = [];
+		anexos.each(function(index2, el2) {
+			fancyboxArgs.push({
+				src: $(el2).children('img').attr('src'),
+				opts: {caption: $(el2).children('figcaption').text()}
+			});
+		});
+
+		$(el).on('click', function(event) {
+			$.fancybox.open(fancyboxArgs, {loop : false});
+			/* Act on the event */
+		});
+	});
+
+	
+
+
+
 });
