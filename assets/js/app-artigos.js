85c8af78037ca5be3686714b2da85efd5a9d20e1
vitaozim <victorhugocgodoi@gmail.com>
2017-06-06 10:19:51 -0300

    Reorganização JS
    - criação de JS só para artigos (header)
    - Edição de todos os htmls para adição do novo JS
    - Remoção do codigo inline dos index para adição do novo JS
    - Remoção do botão de subir

---
+++ assets/js/app-artigos.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+	var corpo = $('body');
+	var titulosVolumes = [
+		"Volume 1: Identidade e Patrimônio Culturais",
+		"Volume 2: Acesso e Participação à Vida cultural &bull; Informação e Comunicação &bull; Responsabilidade dos Atores Públicos",
+		"Volume 3: Educação e Formação &bull; Cooperação Cultural &bull; Princípios de Administração Democrática",
+		"Volume 4: Acesso e Participação 1a Vida Cultural &bull; Informação e Comunicação"
+	];
+
+	var templateHeader = $('\
+		<header id="topo">\
+			<div class="container">\
+				<a href="../../../index.html" class="marca">\
+					<span>Resultados e Contribuições para o Curso de Especialização Interdisciplinar em</span>\
+					<span class="curso-nome">Patrimônio, Direitos Culturais e Cidadania</span>\
+					<span class="livro"></span>\
+				</a>\
+				<a href="../../../sumario.html" class="sumario"><span class="menu"><span>&mdash;</span><span>&mdash;</span><span>&mdash;</span></span>Sumário</a>\
+			</div>\
+		</header>\
+	');
+
+	$.each(titulosVolumes, function(index, val) {
+		 if (corpo.hasClass('livro'+(index+1) ) ) {
+		 	clone = templateHeader.clone();
+		 	clone.find('span.livro').html(val);
+		 	corpo.prepend(clone);
+		 }
+	});
+});
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,5 @@
 jQuery(document).ready(function($) {
 
-	var corpo = $('body');
-	var titulosVolumes = [
-		"Volume 1: Identidade e Patrimônio Culturais",
-		"Volume 2: Acesso e Participação à Vida cultural &bull; Informação e Comunicação &bull; Responsabilidade dos Atores Públicos",
-		"Volume 3: Educação e Formação &bull; Cooperação Cultural &bull; Princípios de Administração Democrática",
-		"Volume 4: Acesso e Participação 1a Vida Cultural &bull; Informação e Comunicação"
-	];
-
-	var templateHeader = $('\
-		<header id="topo">\
-			<div class="container">\
-				<a href="../../../index.html" class="marca">\
-					<span>Resultados e Contribuições para o Curso de Especialização Interdisciplinar em</span>\
-					<span class="curso-nome">Patrimônio, Direitos Culturais e Cidadania</span>\
-					<span class="livro"></span>\
-				</a>\
-				<a href="../../../sumario.html" class="sumario"><span class="menu"><span>&mdash;</span><span>&mdash;</span><span>&mdash;</span></span>Sumário</a>\
-			</div>\
-		</header>\
-	');
-
-	$.each(titulosVolumes, function(index, val) {
-		 if (corpo.hasClass('livro'+(index+1) ) ) {
-		 	clone = templateHeader.clone();
-		 	clone.find('span.livro').html(val);
-		 	corpo.prepend(clone);
-		 }
-	});
-	
-
 	$('#rodape > .container').append('<a href="#subir" class="btn-subir">subir</a>');
 	$('a.btn-subir').on('click', function(){
 	  $('html, body').animate({scrollTop:0},1100);
@@ -38,8 +8,10 @@ jQuery(document).ready(function($) {
 
 	var notasrodape = $('.nota-rodape');
 	var btrodape = $('.numero-rodape');
+	var rodapeAtual;
 
 	btrodape.on('click', function(event) {
+
 		var clicado = $(this).attr('data-numero');
 		var posTop = $(this).position().top;
 		var aSair = notasrodape.filter('.db');
